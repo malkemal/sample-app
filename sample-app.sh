0dcd511740f9b48fa1efb6460b76128c7966b271
malkemal <5026221174@student.its.ac.id>
2025-04-21 12:22:06 +0700

    Changed port from 8080 to 5050.

--- sample-app.sh
+++ sample-app.sh
@@ -1,31 +1,18 @@
 #!/bin/bash
-
-echo "🔧 Membuat folder tempdir..."
-mkdir -p tempdir/templates
-mkdir -p tempdir/static
-
-echo "📂 Menyalin file aplikasi ke tempdir..."
+mkdir -p tempdir/templates tempdir/static
 cp sample_app.py tempdir/
 cp -r templates/* tempdir/templates/
 cp -r static/* tempdir/static/
-
-echo "🛠️  Membuat Dockerfile..."
 cat <<EOF > tempdir/Dockerfile
 FROM python:3
 RUN pip install flask
 COPY ./static /home/myapp/static/
 COPY ./templates /home/myapp/templates/
 COPY sample_app.py /home/myapp/
-EXPOSE 8080
+EXPOSE 5050
 CMD ["python3", "/home/myapp/sample_app.py"]
 EOF
-
-echo "🐳 Membuild Docker image..."
 cd tempdir
 docker build -t sampleapp .
-
-echo "🚀 Menjalankan container..."
-docker run -t -d -p 8080:8080 --name samplerunning sampleapp
-
-echo "📦 Container berjalan:"
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a
